d644980a0f6d84a27beabc624411156d8c78ad39
Arniel C. Fernandez <farniel1588@gmail.com>
2021-10-08 18:02:22 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/leaftime/TestMental.java
@@ -0,0 +1,14 @@
+package com.example.leaftime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestMental extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_mental);
+    }
+}
